8e75500e36e5a265365f4b926260e6c88143aa0e
Fary <farhiya.i.mahamud@gmail.com>
2022-06-03 13:41:41 +0100

    footer components with contact details added

---
+++ react-portfolio/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <footer id="contact-me">
+        <h2>Contact me</h2>
+     <p>farhiya.i.mahamud@gmail.com</p>
+   </footer>
+
+    )
+}
+
+export default Footer
