c8729ccece7398f0b5dd2eae8080e5a32029bdec
Fary <farhiya.i.mahamud@gmail.com>
2022-06-03 13:40:31 +0100

    nav function component added

---
+++ react-portfolio/src/components/nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav () {
+    return (
+    
+        <nav className="nvblack">
+        <ul id="nav-list">
+          <li><a href="#about-me">About Me</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact-me">Contact</a></li>
+        </ul>
+    </nav>    
+    )
+}
+
+export default Nav
